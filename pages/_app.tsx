8998bf1263bb17d956cde18225c11e8158112b90
rhadu <radu.turtoi@gmail.com>
2022-02-27 21:17:06 +0200

    removed i18

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from 'next/app'
 import { FC } from 'react'
-import { appWithTranslation } from 'next-i18next'
 import { withStore } from '../store'
 
 const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
 )
-export default withStore(appWithTranslation(WrappedApp))
+export default withStore(WrappedApp)
