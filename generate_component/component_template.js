e6678800eca8f56f208d764d302d8db27266479f
rhadu <radu.turtoi@gmail.com>
2022-02-27 20:55:36 +0200

    removed comments and improved tsx default template

--- generate_component/component_template.js
+++ generate_component/component_template.js
@@ -1,15 +1,17 @@
 // component.js
-export const component = name => `
-export default function ${name}() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+exports.component = name => `
+  export interface I${name}Props {
+    defaultProp: string;
+  }
+
+  const ${name} = ({}: I${name}Props) => {
+    return <div>Hello , I am a ${name} component.</div>;
+  };
+
+  export default ${name};
 `;
 
 // index.js
-export const barrel = name => `import ${name} from './${name}';
+exports.barrel = name => `import ${name} from './${name}';
 export default ${name};
 `;
--- generate_component/index.js
+++ generate_component/index.js
@@ -1,5 +1,6 @@
-import { existsSync, mkdirSync, writeFile } from 'fs';
-import { component, barrel } from './component_template.js';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const { component, barrel } = require('./component_template.js');
 
 // grab component name from terminal argument
 const [name] = process.argv.slice(2);
@@ -8,41 +9,16 @@ if (!name) throw new Error('You must include a component name.');
 const dir = `./components/${name}/`;
 
 // throw an error if the file already exists
-if (existsSync(dir)) throw new Error('A component with that name already exists.'); 
+if (fs.existsSync(dir)) throw new Error('A component with that name already exists.'); 
 
 // create the folder
-mkdirSync(dir);
+fs.mkdirSync(dir);
 
 function writeFileErrorHandler(err) {
   if (err) throw err;
 }
 
 // component.tsx
-writeFile(`${dir}/${name}.tsx`, component(name), writeFileErrorHandler);
+fs.writeFile(`${dir}/${name}.tsx`, component(name), writeFileErrorHandler);
 // index.tsx
-writeFile(`${dir}/index.tsx`, barrel(name), writeFileErrorHandler);
-
-////////////////
-/// Optional ///
-////////////////
-
-// // insert new component into 'components/index.ts file
-// fs.readFile('./src/components/index.ts', 'utf8', function(err, data) {
-//   if (err) throw err;
-
-//   // grab all components and combine them with new component
-//   const currentComponents = data.match(/(?<=import )(.*?)(?= from)/g);
-//   const newComponents = [name, ...currentComponents].sort();
-
-//   // create the import and export statements
-//   const importStatements = newComponents
-//     .map(importName => `import ${importName} from './${importName}';\n`)
-//     .join('');
-//   const exportStatements = `export {\n${newComponents
-//     .map(component => `  ${component},\n`)
-//     .join('')}};\n`;
-
-//   const fileContent = `${importStatements}\n${exportStatements}`;
-
-//   fs.writeFile(`./src/components/index.ts`, fileContent, writeFileErrorHandler);
-// });
+fs.writeFile(`${dir}/index.ts`, barrel(name), writeFileErrorHandler);
