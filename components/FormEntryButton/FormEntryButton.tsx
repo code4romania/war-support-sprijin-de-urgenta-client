2e1b0ecc7cf2482ab2cd3f6cda58a1f57de73619
rhadu <radu.turtoi@gmail.com>
2022-02-28 10:57:25 +0200

    (feat) Home Component additional styling

--- components/FormEntryButton/FormEntryButton.tsx
+++ components/FormEntryButton/FormEntryButton.tsx
@@ -1,23 +1,26 @@
-import clsx from "clsx";
-import Link from "next/link";
+import clsx from 'clsx'
+import Link from 'next/link'
 
 export interface IFormEntryButtonProps {
-  text: string;
-  route: string;
+  text: string
+  route: string
 }
 
 const FormEntryButton = ({ text, route }: IFormEntryButtonProps) => {
   return (
     <Link href={route} passHref>
-      <div className={clsx('cursor-pointer rounded md px-12 py-7 flex justify-center',
-        'bg-blue-900 text-white',
-        'hover:bg-blue-100 hover:text-black')}>
-        <span className='text-2xl font-semibold'>
-          {text}
-        </span>
+      <div
+        className={clsx(
+          'cursor-pointer rounded md px-12 py-11 flex justify-center',
+          'bg-blue-900 text-white',
+          'hover:bg-blue-100 hover:text-black',
+          'hover:transition-colors hover:duration-200'
+        )}
+      >
+        <span className="text-2xl font-semibold">{text}</span>
       </div>
     </Link>
-  );
-};
+  )
+}
 
-export default FormEntryButton;
+export default FormEntryButton
