a3562b6e32d637d9f481958b1d650c83ae38905a
Virgil Bugnariu <bugnariu.virgil@gmail.com>
2022-02-28 15:24:23 +0200

    Added input component and updated Login page to use it

---
+++ components/Form/Input.tsx
@@ -0,0 +1,50 @@
+import { forwardRef, InputHTMLAttributes } from "react";
+import clsx from "clsx";
+import {ErrorOption} from "react-hook-form";
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement>{
+  label: string;
+  errors: ErrorOption,
+}
+
+// eslint-disable-next-line react/display-name
+const Element = forwardRef<HTMLInputElement, IProps>(({
+  label,
+  value,
+  name,
+  type= 'text',
+  errors,
+  ...rest
+}, ref) => {
+
+  return (
+    <div className={clsx("mb4", {'pb-5': !errors })}>
+      {label && (
+        <label
+          className={clsx('block text-base font-semibold text-gray-700')}
+          htmlFor={name}
+        >
+          {label}
+        </label>
+
+      )}
+
+      <input
+        type={type}
+        name={name}
+        id={name}
+        value={value}
+        ref={ref}
+        className={clsx(
+          'block w-full h-10 mt-1',
+          'px-3 py-2',
+          'border border-gray-100 rounded-md',
+          'focus:ring-blue-600 focus:border-blue-600 focus:border-2 focus:outline-none',
+        )}
+        {...rest}
+      />
+      {errors && <p className="text-sm pl-1 pr-1">{errors.message}</p>}
+    </div>
+  );
+})
+export default Element;
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,60 +2,63 @@ import type { NextPage } from 'next'
 import Button from '@/components/Button'
 import clsx from 'clsx'
 import { useTranslation } from 'react-i18next'
+import Input from '../components/Form/Input';
+import { useForm } from "react-hook-form";
+import { useMemo } from "react";
 
 const LoginPage: NextPage = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+
+  const { handleSubmit, register, formState: { errors } } = useForm();
+
+  // TODO: The actual validation logic will probably be handled by YUP
+  const constraints = useMemo(() => {
+    return ({
+      userName: {
+        required: t('validation.required'),
+      },
+      password: {
+        required: t('validation.required'),
+      }
+    });
+  }, [t]);
+
+  const onSubmit = (values: unknown) => {
+    console.log('values', values)
+  }
 
   return (
     <main className={clsx('grid place-items-center', 'h-screen')}>
       <section className="space-y-16 w-96">
-        <form className="flex flex-col gap-6 ">
-          <div className="">
-            <label
-              htmlFor="userName"
-              className="block text-base font-semibold text-gray-700"
-            >
-              {t('login.userLabel')}
-            </label>
-            <input
-              type="text"
-              name="userName"
-              id="userName"
-              className={clsx(
-                'block w-full h-10 mt-1',
-                'px-3 py-2',
-                'border border-gray-100 rounded-md',
-                'focus:ring-blue-600 focus:border-blue-600 focus:border-2 focus:outline-none'
-              )}
-            />
-          </div>
-          <div className="">
-            <label
-              htmlFor="password"
-              className="block text-base font-semibold text-gray-700"
-            >
-              {t('login.passwordLabel')}
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className={clsx(
-                'block w-full h-10 mt-1',
-                'px-3 py-2',
-                'border border-gray-100 rounded-md',
-                'focus:ring-blue-600 focus:border-blue-600 focus:border-2 focus:outline-none'
-              )}
-            />
-          </div>
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            label={t('login.userLabel')}
+            errors={errors['userName']}
+            {...register('userName', constraints['userName'])}
+          />
+          <Input
+            type="password"
+            label={t('login.passwordLabel')}
+            errors={errors['password']}
+            {...register('password', constraints['password'])}
+          />
           <a href="#" className="self-end text-gray-300 mt-[-16px]">
             {t('login.forgotPass')}
           </a>
+          <div className="space-y-4">
+            <Button
+              type="submit"
+              text={t('login.submit')}
+              size="medium"
+              variant="primary"
+            />
+            <Button
+              text={t('login.signUp')}
+              size="medium"
+              variant="secondary"
+            />
+          </div>
         </form>
-        <div className="space-y-4">
-          <Button text={t('login.submit')} size="medium" variant="primary" />
-          <Button text={t('login.signUp')} size="medium" variant="secondary" />
-        </div>
       </section>
     </main>
   )
