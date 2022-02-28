9e863500d2e55fc0fb097d82bdc6dc3aea5577d1
Virgil Bugnariu <bugnariu.virgil@gmail.com>
2022-02-28 17:38:41 +0200

    Removed global css and used proper tailwind classes to style the elements

--- components/Form/Checkbox.tsx
+++ components/Form/Checkbox.tsx
@@ -1,4 +1,5 @@
 import { FC, InputHTMLAttributes } from "react";
+import clsx from "clsx";
 
 const Element: FC<InputHTMLAttributes<HTMLInputElement>> = ({
   name,
@@ -6,14 +7,29 @@ const Element: FC<InputHTMLAttributes<HTMLInputElement>> = ({
   value,
 }) => {
   return (
-    <div>
+    <div
+      className="flex items-center"
+    >
       <input
         type="checkbox"
         name={name}
         id={name}
         value={value}
+        className={clsx(
+          'appearance-none mr-2',
+          'before:content-[" "] before:block',
+          'w-4 h-4',
+          'border-gray-100 border-2 rounded-sm',
+          'checked:border-blue-600 checked:bg-blue-600',
+          'checked:bg-checkbox-selected bg-center bg-no-repeat',
+        )}
       />
-      <label htmlFor={name}>
+      <label
+        htmlFor={name}
+        className={clsx(
+          'text-sm'
+        )}
+      >
         {children}
       </label>
     </div>
--- components/Form/Radio.tsx
+++ components/Form/Radio.tsx
@@ -1,4 +1,5 @@
 import {FC, InputHTMLAttributes} from "react";
+import clsx from "clsx";
 
 const Element: FC<InputHTMLAttributes<HTMLInputElement>> = ({
   name,
@@ -6,14 +7,29 @@ const Element: FC<InputHTMLAttributes<HTMLInputElement>> = ({
   value,
 }) => {
   return (
-    <div>
+    <div
+      className="flex items-center"
+    >
       <input
         type="radio"
         name={name}
         id={`${name}_${value}`}
         value={value}
+        className={clsx(
+          'appearance-none mr-2',
+          'w-4 h-4',
+          'before:content-[" "] before:block',
+          'border-gray-100 border-2 rounded-lg',
+          'checked:border-blue-600 checked:bg-blue-600',
+          'checked:bg-radio-selected bg-center bg-no-repeat',
+        )}
       />
-      <label htmlFor={`${name}_${value}`}>
+      <label
+        htmlFor={`${name}_${value}`}
+        className={clsx(
+          'text-sm'
+        )}
+      >
         {children}
       </label>
     </div>
