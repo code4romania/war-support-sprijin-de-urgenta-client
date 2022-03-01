033cb695859909ef2e54772c4baebf44ea2b09d7
Virgil B <virgilbugnariu@users.noreply.github.com>
2022-03-01 13:00:05 +0200

    Added Radio and Checkbox group components

---
+++ components/Form/CheckboxGroup.tsx
@@ -0,0 +1,23 @@
+import {FC, InputHTMLAttributes} from "react";
+import {ElementWrapper} from "@/components/Form/common";
+import {ErrorOption} from "react-hook-form";
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label?: string;
+  errors?: ErrorOption;
+}
+
+const CheckboxGroup: FC<IProps> = ({
+  children,
+  errors
+}) => {
+  return (
+    <ElementWrapper hasError={!!errors}>
+      {children}
+      {errors && <p className="text-sm pl-1 pr-1 text-red-50">{errors.message}</p>}
+    </ElementWrapper>
+  )
+}
+
+export default CheckboxGroup;
