e368c8b09dcb03fcb59084ec7d1d8e2436635b47
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-01 21:34:16 +0200

    Add SWR and fetch data on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,22 @@ import Button from '@/components/Button'
 import clsx from 'clsx'
 import type { NextPage } from 'next'
 import { useTranslation } from 'react-i18next'
+import useSWR, { Fetcher } from 'swr'
+
+interface ICategory {
+  pk: number
+  name: string
+}
+
+const fetcher: Fetcher<ICategory[]> = (args: any) =>
+  fetch(args).then((res) => res.json())
 
 const HomePage: NextPage = () => {
   const { t } = useTranslation()
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_PUBLIC_API}/categories_by_name`,
+    fetcher
+  )
   return (
     <main className={clsx('layout h-full')}>
       <section className="mb-24">
@@ -16,10 +29,9 @@ const HomePage: NextPage = () => {
       <div>
         <h2 className="mb-4 text-xl leading-8">{t('wanna.help')}</h2>
         <div className={clsx('grid grid-cols-2 gap-8')}>
-          <Button text={t('housing')} route="/cazare" />
-          <Button text={t('products')} route="/produse" />
-          <Button text={t('services')} route="/servicii" />
-          <Button text={t('others')} route="/altele" />
+          {data?.map((item: ICategory) => (
+            <Button key={item.pk} text={item.name} route="/register" />
+          ))}
         </div>
       </div>
     </main>
