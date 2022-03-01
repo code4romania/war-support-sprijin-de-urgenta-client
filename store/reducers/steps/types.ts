449adadc0933135bf19812ff28e2fd1ec36dc1b6
Diana Băcîrcea <dianabacircea19@gmail.com>
2022-03-01 17:22:14 +0200

    Address feedback

--- store/reducers/steps/types.ts
+++ store/reducers/steps/types.ts
@@ -1,7 +1,13 @@
 import { ReactNode } from 'react'
 
+export enum UserComponentType {
+  userType = 'User Type',
+  userData = 'User Data',
+  userResources = 'User Resources'
+}
+
 export interface Step {
-  component: ReactNode
+  component: UserComponentType
   label: string
 }
 
