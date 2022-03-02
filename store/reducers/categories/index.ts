020749ac4fabf6c7da0672194865e8889f14c955
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-02 11:25:51 +0200

    Hardcode categories (#87)

---
+++ store/reducers/categories/index.ts
@@ -0,0 +1,20 @@
+import { AnyAction } from 'redux'
+
+export const initialState = [
+  {
+    slug: 'services',
+  },
+  {
+    slug: 'products',
+  },
+  {
+    slug: 'volunteer',
+  },
+  {
+    slug: 'others',
+  }
+]
+
+export const categories = (state = initialState, action: AnyAction) => {
+  return state
+}
