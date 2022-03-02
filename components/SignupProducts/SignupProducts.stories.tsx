6585952761ad7969c60407dd6cbebdfcf7a32430
Virgil B <virgilbugnariu@users.noreply.github.com>
2022-03-02 10:03:01 +0200

    Formatting

--- components/SignupProducts/SignupProducts.stories.tsx
+++ components/SignupProducts/SignupProducts.stories.tsx
@@ -1,22 +1,21 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import SignupProducts from '.'
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import SignUpProducts from './SignUpProducts';
 
 export default {
-  title: 'Signup/Products',
-  component: SignupProducts,
-} as ComponentMeta<typeof SignupProducts>
+  title: 'SignUpProducts',
+  component: SignUpProducts
+} as ComponentMeta<typeof SignUpProducts>;
 
-const Template: ComponentStory<typeof SignupProducts> = (args) => {
-  return (
-    <SignupProducts {...args} />
-  )
-}
-export const Default = Template.bind({})
+const Template: ComponentStory<typeof SignUpProducts> = (args) => <SignUpProducts {...args} />;
+
+export const Default = Template.bind({});
 
 Default.parameters = {
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/OlNwOwDItLPKBcy5OEROVj/Sprijin-de-urgenta?node-id=57%3A1604',
+    url: 'https://www.figma.com/file/OlNwOwDItLPKBcy5OEROVj/Sprijin-de-urgenta?node-id=309%3A6731',
   },
 }
