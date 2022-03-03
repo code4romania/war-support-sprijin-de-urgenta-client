3b7961311e21c903b300931a8df93e0c59d6fdef
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-03 16:40:30 +0200

    Submit transport data

--- api/types.ts
+++ api/types.ts
@@ -1,25 +1,32 @@
 export type TransportServicesRequest = {
-    weight_capacity?: number;
-    weight_unit?: string;
-    has_refrigeration?: boolean;
-    type?: string;
-    county_coverage?: string;
-    availability?: string;
-    availability_interval_from?: Date;
-    availability_interval_to?: Date;
-    driver_name: string;
-    driver_id: string;
-    driver_contact: string;
-    car_registration_number: string;
-    available_seats?: number;
-    has_disabled_access?: boolean;
-    pets_allowed?: boolean;
-    status?: number;
-    donor?: string;
-    category?: string;
+  weight_capacity?: number
+  weight_unit?: string
+  has_refrigeration?: boolean
+  type?: string
+  county_coverage?: string
+  availability?: string
+  availability_interval_from?: Date
+  availability_interval_to?: Date
+  driver_name: string
+  driver_id: string
+  driver_contact: string
+  car_registration_number: string
+  available_seats?: number
+  has_disabled_access?: boolean
+  pets_allowed?: boolean
+  status?: number
+  donor?: string
+  category?: string
 }
 
 export enum TransportType {
-  National = "1",
-  County = "2"
+  National = '1',
+  County = '2',
+}
+
+export enum AvailabilityType {
+  Weekend = 'WK',
+  Weekdays = 'WD',
+  Anytime = 'A',
+  FixedIntervals = 'FI',
 }
