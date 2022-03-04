65e347ca8d45c70350e8260d8bade0be00d255d2
SergiuPintilei <pintileisergiu@gmail.com>
2022-03-04 21:27:19 +0200

    redirect to "/offer/resources" for offer flow (#165)

--- components/SignupPage/SignupPage.tsx
+++ components/SignupPage/SignupPage.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux'
 import { State } from '@/store/types/state.type'
-import { UserComponentType } from '@/store/reducers/steps/types'
+
 import Spacer from '@/components/Spacer'
 import Stepper from '@/components/Stepper'
 import UserTypeForm from '@/components/UserTypeForm'
@@ -10,27 +10,23 @@ export interface ISignupPageProps {
   resourceType: string
 }
 
-const componentMap = {
-  [UserComponentType.userType]: <UserTypeForm />,
-  [UserComponentType.userData]: <UserCredentials />,
-}
-
 const SignupPage = ({ resourceType }: ISignupPageProps) => {
   const steps = useSelector((state: State) => state.steps.steps)
   const activeStep = useSelector((state: State) => state.steps.activeStep)
-  const type = resourceType === 'offer' ? 'offer' : 'request'
 
   const currentComponent =
-    activeStep === 0
-      ? componentMap[UserComponentType.userType]
-      : componentMap[UserComponentType.userData]
+    activeStep === 0 ? (
+      <UserTypeForm />
+    ) : (
+      <UserCredentials resourceType={resourceType} />
+    )
 
   return (
     <main className="container md:mx-auto">
       <Spacer size="3rem" />
       <Stepper
         activeStep={activeStep}
-        steps={steps[type].map((step) => step.label)}
+        steps={steps[resourceType].map((step) => step.label)}
       />
       <Spacer size="4rem" />
       <div className="px-3 ">{currentComponent}</div>
