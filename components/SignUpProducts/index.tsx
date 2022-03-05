04e14dd9c2c1e16e90d7a72097f7cea9661808f1
Cosmin Leferman <14358537+cleferman@users.noreply.github.com>
2022-03-05 03:39:14 +0200

    Integrated resource table with services. Products still needs work

--- components/SignUpProducts/index.tsx
+++ components/SignUpProducts/index.tsx
@@ -1,18 +1,16 @@
-import clsx from 'clsx'
-import { useTranslation } from 'react-i18next'
-import React, { ReactNode, useState } from 'react'
-
-import Button from '../Button'
-import Form from '@/components/SignUpProducts/Form'
 import { useProductsForm } from '@/hooks/useData'
-import { TransportServicesRequest } from 'api'
+import clsx from 'clsx'
+import React, { ReactNode, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import Button from '../Button'
 import Dialog from '../Dialog'
-import Others from './Others'
-import { TransportPersonsForm } from 'forms'
-import GenericProduct from './GenericProduct'
-import TextileProduct from './TextileProduct'
+import ResourcesTableList from '../ResourcesTableList'
 import BuildingMaterials from './BuildingMaterials'
+import GenericProduct from './GenericProduct'
+import Others from './Others'
 import Tents from './Tents'
+import TextileProduct from './TextileProduct'
+
 
 export interface ISignUpProductsProps {
   defaultProp?: string
@@ -62,9 +60,32 @@ const PRODUCTS: IProductsProps[] = [
   },
 ]
 
-const SignUpProducts = ({}: ISignUpProductsProps) => {
+//TODO: remove this when implementing the form
+type Product = {
+  id: string
+  name: string
+  quantity: number
+  um: string
+}
+
+const SignUpProducts = ({ }: ISignUpProductsProps) => {
   const { t } = useTranslation()
   const { data } = useProductsForm()
+
+  const [productsList, setProductsList] = useState<Product[]>([])
+
+  const resourceTableColumns = [
+    t('resources.product'),
+    t('resources.quantity'),
+  ];
+
+  const onProductRemoved = (id: string) => {
+    const index = productsList.findIndex(t => t.id === id)
+    if (index > -1) {
+      productsList.splice(index, 1)
+      setProductsList(productsList)
+    }
+  }
 
   const [showDialog, setShowDialog] = useState(false)
   const [dialogProductResourceType, setDialogProductResourceType] =
@@ -104,30 +125,38 @@ const SignUpProducts = ({}: ISignUpProductsProps) => {
       className={clsx(
         'container grid place-items-start',
         'bg-blue-50 rounded',
-        'px-8 py-7 md:w-1/2'
+        'px-8 py-7 w-full'
       )}
     >
-      {PRODUCTS.length > 0 &&
-        PRODUCTS.map(
-          ({ resourceType, label }: IProductsProps, index: number) => (
-            <React.Fragment key={`${resourceType}_${label}_${index}`}>
-              <div className="flex items-center gap-4 mb-8 w-full">
-                <h3 className="min-w-fit flex-1">{t(label)}</h3>
-                <Button
-                  text={t('add')}
-                  size="small"
-                  className="flex-1"
-                  variant="tertiary"
-                  onClick={() => {
-                    setShowDialog(true)
-                    setDialogProductResourceType(resourceType)
-                  }}
-                />
-              </div>
-            </React.Fragment>
-          )
-        )}
-      {!!dialogProductResourceType && renderDialog(dialogProductResourceType)}
+      <section className={clsx('flex flex-col md:flex-row w-full')}>
+        <div className='w-full md:w-1/2'>
+          {PRODUCTS.length > 0 &&
+            PRODUCTS.map(
+              ({ resourceType, label }: IProductsProps, index: number) => (
+                <React.Fragment key={`${resourceType}_${label}_${index}`}>
+                  <div className="flex items-center gap-4 mb-8 w-full">
+                    <h3 className="min-w-fit flex-1">{t(label)}</h3>
+                    <Button
+                      text={t('add')}
+                      size="small"
+                      className="flex-1"
+                      variant="tertiary"
+                      onClick={() => {
+                        setShowDialog(true)
+                        setDialogProductResourceType(resourceType)
+                      }}
+                    />
+                  </div>
+                </React.Fragment>
+              )
+            )}
+        </div>
+        <ResourcesTableList className='w-full md:w-1/2 ml-0 md:ml-4' title={t('resources.added.products')}
+          columns={resourceTableColumns}
+          list={productsList.map(t => ({ id: t.id, name: t.name, quantity: t.quantity, um: t.um }))}
+          onItemRemoved={onProductRemoved} />
+        {!!dialogProductResourceType && renderDialog(dialogProductResourceType)}
+      </section>
     </main>
   )
 }
--- components/SignUpServicesForm/SignUpServicesForm.tsx
+++ components/SignUpServicesForm/SignUpServicesForm.tsx
@@ -6,6 +6,7 @@ import { TransportPersonsForm } from 'forms/TransportPersonsForm'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import Dialog from '../Dialog'
+import ResourcesTableList from '../ResourcesTableList'
 
 export const SignUpServicesForm = () => {
   const { t } = useTranslation()
@@ -15,16 +16,26 @@ export const SignUpServicesForm = () => {
     'transportGoods' | 'transportPersons'
   >()
 
+  const [goodsList, setGoodsList] = useState<TransportServicesRequest[]>([])
+  const [personsTransportList, setPersonsTransportList] = useState<TransportServicesRequest[]>([])
+
   const onTransportGoodsSubmit = (data: TransportServicesRequest) => {
-    console.log(data)
+    setGoodsList((state) => [...state, data])
     handleDialogDismiss()
   }
 
   const onTransporPersonsSubmit = (data: TransportServicesRequest) => {
-    console.log(data)
+    setPersonsTransportList((state => [...state, data]))
     handleDialogDismiss()
   }
 
+  const onGoodsRemoved = (itemName: string) => {
+    const index = goodsList.findIndex(t => t.driver_name === itemName)
+    if (index > -1) {
+      goodsList.splice(index, 1)
+      setGoodsList(goodsList)
+    }
+  }
   const handleDialogDismiss = () => {
     setShowDialog(false)
     setShowForm(undefined)
@@ -39,31 +50,45 @@ export const SignUpServicesForm = () => {
       )}
     >
       <section className={clsx('w-full md:w-1/2')}>
-        <div className="flex items-center gap-4 mb-8 w-full">
-          <h3 className="min-w-fit flex-1 mb-4 text-lg font-semibold">{t('services.transport-goods')}</h3>
-          <Button
-            text={t('add')}
-            size="small"
-            className='flex-1'
-            variant="tertiary"
-            onClick={() => {
-              setShowForm('transportGoods')
-              setShowDialog(true)
-            }}
-          />
+        <div className="flex flex-col items-center gap-4 mb-8 w-full">
+          <div className='flex w-full items-center gap-4'>
+            <h3 className="min-w-fit flex-1  text-lg font-semibold">{t('services.transport-goods')}</h3>
+            <Button
+              text={t('add')}
+              size="small"
+              className='flex-1'
+              variant="tertiary"
+              onClick={() => {
+                setShowForm('transportGoods')
+                setShowDialog(true)
+              }}
+            />
+          </div>
+          {goodsList.length > 0 &&
+            <ResourcesTableList
+              columns={[t('services.driver-name')]}
+              list={goodsList.map(t => ({ id: t.driver_name, name: t.driver_name }))}
+              onItemRemoved={onGoodsRemoved} />}
         </div>
-        <div className="flex items-center gap-4 mb-8 w-full">
-          <h3 className="min-w-fit flex-1 mb-4 text-lg font-semibold">{t('services.transport-people')}</h3>
-          <Button
-            text={t('add')}
-            className='flex-1'
-            size="small"
-            variant="tertiary"
-            onClick={() => {
-              setShowForm('transportPersons')
-              setShowDialog(true)
-            }}
-          />
+        <div className="flex flex-col items-center gap-4 mb-8 w-full">
+          <div className='flex w-full items-center gap-4'>
+            <h3 className="min-w-fit flex-1 text-lg font-semibold">{t('services.transport-people')}</h3>
+            <Button
+              text={t('add')}
+              className='flex-1'
+              size="small"
+              variant="tertiary"
+              onClick={() => {
+                setShowForm('transportPersons')
+                setShowDialog(true)
+              }}
+            />
+          </div>
+          {personsTransportList.length > 0 &&
+            <ResourcesTableList
+              columns={[t('services.driver-name')]}
+              list={personsTransportList.map(t => ({ id: t.driver_name, name: t.driver_name }))}
+              onItemRemoved={onGoodsRemoved} />}
         </div>
       </section>
       {/* TODO: The content of the dialog should be dynamically set based on the type of service selected via button  */}
