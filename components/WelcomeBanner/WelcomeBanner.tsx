e96920d9f71152ee8c7d146ab64f0b7183af5c89
rhadu <radu.turtoi@gmail.com>
2022-03-06 20:57:33 +0200

    removed Hero Animation

--- components/WelcomeBanner/WelcomeBanner.tsx
+++ components/WelcomeBanner/WelcomeBanner.tsx
@@ -2,16 +2,12 @@ import { useTranslation } from 'react-i18next'
 import Image from '@/components/Image'
 import clsx from 'clsx'
 import Spacer from '../Spacer'
-import { motion } from 'framer-motion'
 
 const WelcomeBanner = ({}) => {
   const { t } = useTranslation('common')
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 150 }}
-      animate={{ opacity: 1, y: 0 }}
-      
+    <div
       className={clsx(
         'flex flex-col-reverse gap-5 justify-between',
         'md:flex-row'
@@ -31,7 +27,7 @@ const WelcomeBanner = ({}) => {
         alt="Code 4 Romania logo"
         className="md:max-w-[426px]"
       />
-    </motion.div>
+    </div>
   )
 }
 
