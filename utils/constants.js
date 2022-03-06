cc9fadf4ad31c39380ed7c67a5d61acf15faee5c
Radu Turtoi <radu.turtoi@gmail.com>
2022-03-07 00:19:45 +0200

    Add animations to Dialogs (#225)
    * added framer motion and intersection observer
    
    * dialog large screen animations
    
    * refactor dialog on resources form
    
    * Dialog media animations
    
    * removed Hero Animation
    
    * handle Dialog backdrop click

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const BREAKPOINTS = {
+  sm: '640',
+  md: '768',
+  lg: '1024',
+  xl: '1280',
+}
+
+export const QUERIES = {
+  sm: `(min-width: ${BREAKPOINTS.sm / 16}rem)`,
+  md: `(min-width: ${BREAKPOINTS.md / 16}rem)`,
+  lg: `(min-width: ${BREAKPOINTS.lg / 16}rem)`,
+  xl: `(min-width: ${BREAKPOINTS.xl / 16}rem)`,
+}
