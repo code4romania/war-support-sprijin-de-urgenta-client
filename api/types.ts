faadc931214ffc7dd93bdc8b0f20f523fec33bb4
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-07 03:53:51 +0200

    Update request transports

--- api/types.ts
+++ api/types.ts
@@ -24,18 +24,10 @@ export type RequestTransportServicesRequest = {
   weight_capacity?: number
   weight_unit?: string
   has_refrigeration?: boolean
-  type?: string
   from_county: string
   from_city: string
   to_county: string
   to_city: string
-  availability?: string
-  availability_interval_from?: Date
-  availability_interval_to?: Date
-  driver_name: string
-  driver_id: string
-  driver_contact: string
-  car_registration_number: string
   available_seats?: number
   has_disabled_access?: boolean
   pets_allowed?: boolean
