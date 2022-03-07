34976685297652cd265eba9c6ce6642afc0b63a6
Ion Dormenco <idormenco@gmail.com>
2022-03-07 15:03:31 +0200

    Update UserCredentials.tsx

--- components/UserCredentials/UserCredentials.tsx
+++ components/UserCredentials/UserCredentials.tsx
@@ -141,14 +141,14 @@ const UserCredentials = ({ resourceType }: UserCredentialsProps) => {
               {...register(input.name)}
             />
           ))}
-        </div>
-        {serverErrors['non_field_errors']?.map(
+          {serverErrors['non_field_errors']?.map(
             (error: string, index: number) => (
               <div key={index} className={'bg-red-50 p-1 px-2 text-white'}>
                 {error}
               </div>
             )
           )}
+        </div>
       </div>
 
       <div className={`bg-blue-50 px-4 py-4 rounded-md mt-4`}>
