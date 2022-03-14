dddc7d4c0b62c881592ff9f4c3f5d9b01415554e
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-15 00:49:20 +0200

    Update content

--- pages/terms-and-conditions/index.tsx
+++ pages/terms-and-conditions/index.tsx
@@ -12,103 +12,86 @@ const TermsConditions = () => {
       <div className="layout">
         <Spacer size="3.5rem" />
         <h1 className="text-3xl font-bold leading-normal md:max-w-[30ch]">
-          {t('cookie.policy.title')}
+          {t('terms-and-conditions.title')}
         </h1>
         <Spacer size="3.5rem" />
-        <p className="mb-4">{t('cookie.policy.p1')}</p>
-        <p className="mb-4">{t('cookie.policy.p2')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p1')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p2')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p3')}</p>
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.whatiscookie.header')}
+          {t('terms-and-conditions.p4.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.whatiscookie.p1')}</p>
-        <p className="mb-4">{t('cookie.policy.whatiscookie.p2')}</p>
-        <p className="mb-4">{t('cookie.policy.whatiscookie.p3')}</p>
-        <p className="mb-4">{t('cookie.policy.whatiscookie.p4')}</p>
-        <p className="mb-4">{t('cookie.policy.whatiscookie.p5')}</p>
-        <h3 className="text-lg font-semibold">
-          {t('cookie.policy.p3.header')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.p3.content')}</p>
-        <h3 className="text-lg font-semibold">
-          {t('cookie.policy.p4.header')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.p4.content')}</p>
-        <h3 className="text-lg font-semibold">
-          {t('cookie.policy.p5.header')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.p5.content')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p4.content.p1')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p4.content.p2')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p4.content.p3')}</p>
 
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.p6.header')}
+          {t('terms-and-conditions.p5.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.p6.content')}</p>
-        <ul className="w-full py-5 md:w-1/2 md:px-3 md:py-0">
-          <li>{t('cookie.policy.type.general')}</li>
-          <li>{t('cookie.policy.type.registration')}</li>
-          <li>{t('cookie.policy.type.thirdparty')}</li>
+        <p className="mb-4">{t('terms-and-conditions.p5.content')}</p>
+
+        <ul className="list-disc mb-4 pl-5 w-full py-5 md:w-1/2 md:py-0">
+          <li>{t('terms-and-conditions.p5.content.li1')}</li>
+          <li>{t('terms-and-conditions.p5.content.li2')}</li>
+          <li>{t('terms-and-conditions.p5.content.li3')}</li>
         </ul>
-        <h3 className="mt-4 text-lg font-semibold">
-          {t('cookie.policy.type.general')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.type.general.content')}</p>
-        <h3 className="text-lg font-semibold">
-          {t('cookie.policy.type.registration')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.type.registration.content')}</p>
-        <h3 className="text-lg font-semibold">
-          {t('cookie.policy.type.thirdparty')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.type.thirdparty.content')}</p>
 
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.p7.header')}
+          {t('terms-and-conditions.p6.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.p7.content')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p6.content')}</p>
 
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.p8.header')}
+          {t('terms-and-conditions.p7.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.p8.content')}</p>
-        <p className="mb-4">{t('cookie.policy.p9')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p7.content')}</p>
 
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.security.header')}
+          {t('terms-and-conditions.p8.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.security.p1')}</p>
-        <p className="mb-4">{t('cookie.policy.security.p2')}</p>
-        <p className="mb-4">{t('cookie.policy.security.p3')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p8.content')}</p>
 
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.other.header')}
+          {t('terms-and-conditions.p9.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.other.p1')}</p>
-        <p className="mb-4">{t('cookie.policy.other.p2')}</p>
-
-        <h3 className="text-lg font-semibold">
-          {t('cookie.policy.advice.header')}
-        </h3>
-        <p className="mb-4">{t('cookie.policy.advice.p1')}</p>
-        <p className="mb-4">{t('cookie.policy.advice.p2')}</p>
-        <ul className="w-full py-5 md:w-1/2 md:px-3 md:py-0">
-          <li className="mb-3">{t('cookie.policy.advice.p1.1')}</li>
-          <li className="mb-3">{t('cookie.policy.advice.p1.2')}</li>
-          <li className="mb-3">{t('cookie.policy.advice.p1.3')}</li>
+        <p className="mb-4">{t('terms-and-conditions.p9.content')}</p>
+        <ul className="list-disc mb-4 pl-5 w-full py-5 md:w-1/2 md:py-0">
+          <li>{t('terms-and-conditions.p9.content.li1')}</li>
+          <li>{t('terms-and-conditions.p9.content.li2')}</li>
+          <li>{t('terms-and-conditions.p9.content.li3')}</li>
+          <li>{t('terms-and-conditions.p9.content.li4')}</li>
+          <li>{t('terms-and-conditions.p9.content.li5')}</li>
+          <li>{t('terms-and-conditions.p9.content.li6')}</li>
         </ul>
-        <p className="mb-4">{t('cookie.policy.advice.p3')}</p>
 
         <h3 className="text-lg font-semibold">
-          {t('cookie.policy.turnoff.header')}
+          {t('terms-and-conditions.p10.header')}
         </h3>
-        <p className="mb-4">{t('cookie.policy.turnoff.p1')}</p>
-        <p className="mb-4">{t('cookie.policy.turnoff.p2')}</p>
-        <p className="mb-10">{t('cookie.policy.turnoff.p3')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p10.content.p1')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p10.content.p2')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p10.content.p3')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p10.content.p4')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p10.content.p5')}</p>
 
-        <p className="mb-4">
-          {t('cookie.policy.p10')}
-          <a href="http://www.youronlinechoices.com/ro/">
-            http://www.youronlinechoices.com/ro/
-          </a>
-        </p>
+        <h3 className="text-lg font-semibold">
+          {t('terms-and-conditions.p11.header')}
+        </h3>
+        <p className="mb-4">{t('terms-and-conditions.p11.content.p1')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p11.content.p2')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p11.content.p3')}</p>
+
+        <h3 className="text-lg font-semibold">
+          {t('terms-and-conditions.p12.header')}
+        </h3>
+        <p className="mb-4">{t('terms-and-conditions.p12.content')}</p>
+
+        <h3 className="text-lg font-semibold">
+          {t('terms-and-conditions.p13.header')}
+        </h3>
+        <p className="mb-4">{t('terms-and-conditions.p13.content.p1')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p13.content.p2')}</p>
+        <p className="mb-4">{t('terms-and-conditions.p13.content.p3')}</p>
+
         <Spacer size="3.5rem" />
       </div>
     </>
