e82188f046b15a604277e140a65c620e6e2ee154
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-17 15:57:47 +0200

    Update phone number validator

--- utils/regexes.ts
+++ utils/regexes.ts
@@ -1,3 +1,3 @@
-export const phoneNumberRegex = /^(\+|00)[1-9][0-9 \-\(\)\.]{7,32}$/;
+export const phoneNumberRegex = /^[a-zA-Z0-9()+-]{7,32}$/
 export const roCarRegistrationNumber = /[A-Za-z]{1,2}[0-9]{2,3}[A-Za-z]{3}/
 export const roIdentityCardRegex = /[A-Za-z]{1,2}[0-9]{6}/
