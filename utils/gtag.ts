1bf604634e00eefd6f05432e3abcbf237f50e762
Utwo <mihai.legat@gmail.com>
2022-04-27 16:53:27 +0300

    Remove NEXT_PUBLIC_GA_ID from env

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || ''
+export const GA_TRACKING_ID = 'G-W7RC2D7RJP'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
